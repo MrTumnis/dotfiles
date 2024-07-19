3b085c7547810c6cb0db4e1e5e5b3e6babaf819d
mrtumnis <thomas.hefferman1@gmail.com>
2024-07-19 11:51:40 -0600

    update ansible.sh

--- run_once_install_ansible.sh
+++ run_once_install_ansible.sh
@@ -9,29 +9,20 @@ install_on_void() {
     sudo xbps-install -S ansible
 }
 
-install_on_mac() {
-    homebrew install ansible
-}
-
 OS="$(uname -s)"
 case "${OS}" in
     Linux*)
         if [ -f /etc/fedora-release ]; then
             install_on_fedora
-        elif [ -f /etc/lsb-release ]; then
+        elif [ -f /etc/os-release ]; then
             install_on_void
         else
             echo "Unsupported Linux distribution"
             exit 1
-        fi
-        ;;
-    Darwin*)
-        install_on_mac
-        ;;
-    *)
-        echo "Unsupported operating system: ${OS}"
-        exit 1
-        ;;
+	fi
+	exit 1
+
+	;;
 esac
 
 
