da2420b7746413e9a59dd90dc44d82c534c9d018
MrTumnis <138890915+MrTumnis@users.noreply.github.com>
2024-08-03 16:44:56 -0600

    update script

--- run_once_install_ansible.sh
+++ run_once_install_ansible.sh
@@ -5,7 +5,7 @@ install_on_fedora() {
 }
 
 install_on_void() {
-   sudo xbps-install -Suv && sudo xbps-install -S ansible
+    sudo xbps-install -Suv && sudo xbps-install -S ansible
 }
 
 OS="$(uname -s)"
@@ -13,18 +13,29 @@ case "${OS}" in
     Linux*)
         if [ -f /etc/fedora-release ]; then
             install_on_fedora
-        elif [ -f /etc/os-release ]; then
+        elif grep -q 'Void' /etc/os-release; then
             install_on_void
         else
             echo "Unsupported Linux distribution"
-            exit 0
-	fi
-	exit 1
-
-	;;
+            exit 1
+        fi
+        ;;
+    *)
+        echo "Unsupported OS"
+        exit 1
+        ;;
 esac
 
-ansible-playbook ~/.void_setup/bootstrap/setup.yml --ask-become-pass
-
-echo "Ansible installation complete."
-
+# Run the Ansible playbook if Ansible was installed successfully
+if command -v ansible-playbook > /dev/null; then
+    ansible-playbook ~/.void_setup/bootstrap/setup.yml --ask-become-pass
+    if [ $? -eq 0 ]; then
+        echo "Ansible playbook executed successfully."
+    else
+        echo "Failed to execute Ansible playbook."
+        exit 1
+    fi
+else
+    echo "Ansible installation failed."
+    exit 1
+fi
