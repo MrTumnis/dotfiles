273edf1c66c2160684fcb27f3d3c35bd6ecf39d0
mrtumnis <thomas.hefferman1@gmail.com>
2024-08-03 21:52:40 -0600

    add suckless config

---
+++ dot_config/suckless/dwmblocks-distrotube/blocks.def.h
@@ -0,0 +1,26 @@
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+static const Block blocks[] = {
+	/*Icon*/ /*Command*/	 	                            /*Update Interval*/	/*Update Signal*/
+      //  {" 🐧 ", "~/.scripts/distrotube/kernel",	 360,		          2},
+
+     // {" 🔺 ", "~/.scripts/distrotube/upt",		 60,		          2},
+
+	{" 📦 ", "~/.scripts/distrotube/xbpsupdate",  360,		          9},
+	
+	{" 💻 ", "~/.scripts/distrotube/memory",	 6,		              1},
+
+	{" 🔊 ", "~/.scripts/distrotube/volume",     0,		              10},
+
+	{" 🕑 ", "~/.scripts/distrotube/clock",	     60,	              0},
+
+	{"    ", "~/.scripts/LS/sb-internet",	     5,	              4},
+	
+	{"    ", "~/.scripts/LS/sb-battery",	     5,	              3},
+	
+	{"    ", "~/.scripts/LS/sb-doppler",	     0,	             13},
+	
+	{"    ", "~/.scripts/LS/sb-forecast",	     18000,	              5},
+};
+
+//sets delimeter between status commands. NULL character ('\0') means no delimeter.
+static char delim = ':';
