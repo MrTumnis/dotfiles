cee217f18e4814871b2157f4379c719ec04e10fa
thomas <thomas@Errai.tail7f2ded.ts.net>
2025-03-07 16:09:20 -0700

    update dots

--- dot_config/yazi/init.lua
+++ dot_config/yazi/init.lua
@@ -4,3 +4,26 @@ require("full-border"):setup {
 }
 
 require("git"):setup()
+require("no-status"):setup()
+require("augment-command"):setup {
+	prompt = false,
+	default_item_group_for_prompt = "hovered",
+	smart_enter = true,
+	smart_paste = true,
+	smart_tab_create = false,
+	smart_tab_switch = false,
+	confirm_on_quit = false,
+	open_file_after_creation = false,
+	enter_directory_after_creation = false,
+	use_default_create_behaviour = false,
+	enter_archives = true,
+	extract_retries = 3,
+	recursively_extract_archives = true,
+	preserve_file_permissions = false,
+	must_have_hovered_item = true,
+	skip_single_subdirectory_on_enter = true,
+	skip_single_subdirectory_on_leave = true,
+	smooth_scrolling = false,
+	scroll_delay = 0.02,
+	wraparound_file_navigation = false,
+}
